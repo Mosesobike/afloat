37a008a0eb6cb1d199189a64277974b738ebc1eb
moses <mosesobike@gmail.com>
2020-11-24 21:00:41 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const tl = gsap.timeline({default: {ease: "power1.out"}});
+
+
+tl.to('.text', {y:'0%', duration: 1, stagger: 0.50}); 
+tl.to(".slider", {y: '-100%', duration: 1.5});
+tl.to(".intro",{y:"-100%",duration : 1},"-=1.5");
+tl.fromTo("nav", {opacity :0}, {opacity:1, duration:1},'-=1');
+tl.fromTo(".big-text", {opacity :0}, {opacity:1, duration:1},);
+
+
+const hamburger = document.querySelector(".hamburger");
+const navlinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+
+hamburger.addEventListener("click", () => {
+    navlinks.classList.toggle("open");
+
+});
